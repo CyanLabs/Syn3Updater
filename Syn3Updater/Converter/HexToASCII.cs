8a872b70d0ea8e3ec962a439c0c0bfd6265a700b
Fma965 <fma965@cyanlabs.net>
2021-09-19 18:13:11 +0100

    Fixed #154 - Crash while parsing log

--- Syn3Updater/Converter/HexToASCII.cs
+++ Syn3Updater/Converter/HexToASCII.cs
@@ -1,28 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Cyanlabs.Syn3Updater.Converter
 {
-    public class SyncHexToAscii
+    public static class SyncHexToAscii
     {
         public static List<string> ConvertPackages( string packageHex)
         {
-            List<string> output = new();
-            foreach (string package in Regex.Replace(packageHex, "0*0", " ").Split(' '))
+            string res = string.Empty;
+            for (int a = 0; a <packageHex.Length ; a += 2)
             {
-                string res = string.Empty ;
-                for (int a = 0; a <package.Length ; a += 2)
+                string char2Convert = packageHex.Substring(a, 2);
+                if (char2Convert == "00") 
                 {
-                    string char2Convert = package.Substring(a, 2);
+                    res += "_";
+                }
+                else
+                {
                     int n = Convert.ToInt32(char2Convert, 16);
                     char c = (char)n;
                     res += c.ToString();
                 }
-                if (res != string.Empty) output.Add(res);
             }
-            return output;
+            return Regex.Replace(res, "_*_", "_").Split('_').ToList();
         }
-
     }
 }
